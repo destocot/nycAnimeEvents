d78986b93b000d0668419a86dcc9f2c24a129549
Khurram Ali <khurramcali@gmail.com>
2024-09-30 20:17:32 -0400

    added more evnets

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const events = [
     title: "AXCN: Mobile Suit Gundam",
     date: [new Date("2024-10-02"), new Date("2024-10-06")],
     description:
-      "In Universal Century year 0078, the space colony Side 3 declares itself the Principality of Zeon and launches a war of independence against the Earth Federation.",
+      "In Universal Century year 0078, the space colony Side 3 declares itself the Principality of Zeon and launches a war of independence against the Earth Federation. Eight months have passed since the war entered into a deadlock.",
     image:
       "https://d2alu56i91c6gw.cloudfront.net/iconicreleasing/MSG1_720x1015_A1.jpg",
     href: "https://www.iconicreleasing.com/events/mobile-suit-gundam/tickets/",
@@ -54,22 +54,37 @@ const events = [
     href: "https://kodansha.us/manga-meets/#events",
   },
   {
-    title: '"Toast of Tardiness" Challenge',
-    date: [new Date("2024-10-06")],
+    title: "AXCN: Mobile Suit Gundam II: Soldiers of Sorrow",
+    date: [new Date("2024-10-16"), new Date("2024-10-20")],
     description:
-      "3 P.M. A manga-themed scavenger hunt from Midtown Manhattan to Kodansha House!",
+      "With evacuees from Side 7 onboard, the White Base crosses the Pacific Ocean and heads west towards the continent of Eurasia. Amidst that journey filled with battle, Amuro has a fight with Bright and runs away, suffering from the deaths of Matilda, who he had feelings for, and Ryu, who was like a big brother to him.",
     image:
-      "https://imagedelivery.net/gbfTL37HXewiIoqsZv9WzQ/78e95134-6954-4428-3c56-acb370767a00/public",
-    href: "https://kodansha.us/manga-meets/#events",
+      "https://d2alu56i91c6gw.cloudfront.net/iconicreleasing/GUNDAM2_720x1015_A1.jpg",
+    href: "https://www.iconicreleasing.com/events/mobile-suit-gundam-2/tickets/",
+  },
+  {
+    title: "AXCN: Mobile Suit Gundam III: Encounters in Space",
+    date: [new Date("2024-10-23"), new Date("2024-10-27")],
+    description:
+      "Having officially been inducted into the Federation Forces as the 13th Independent Unit, the White Base enters the neutral colony Side 6 to serve as a decoy. There Amuro reunites with his father Tem, and meets a mysterious girl named Lalah Sune.",
+    image:
+      "https://d2alu56i91c6gw.cloudfront.net/iconicreleasing/GUNDAM3_720x1015_A1.jpg",
+    href: "https://www.iconicreleasing.com/events/mobile-suit-gundam-2/tickets/",
   },
 ];
 
 // filter if date.at(-1) is before today
 
 const today = new Date();
-const filteredEvents = events.filter(
-  (event) => event.date[event.date.length - 1] > today
-);
+const filteredEvents = events.filter((event) => {
+  const isAfterToday = event.date[event.date.length - 1] > today;
+
+  if (!isAfterToday) {
+    console.log(`Filtered out event: ${event.title}`);
+  }
+
+  return isAfterToday;
+});
 
 filteredEvents.sort((a, b) => a.date[0].getTime() - b.date[0].getTime());
 
