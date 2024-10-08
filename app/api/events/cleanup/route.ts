18c7dfa321356fc351375710db99e0e37c4642d3
Khurram Ali <khurramcali@gmail.com>
2024-10-08 01:23:26 -0400

    test sunday deletions

--- app/api/events/cleanup/route.ts
+++ app/api/events/cleanup/route.ts
@@ -1,4 +1,6 @@
 import db from '@/lib/db'
+import { EventDate } from '@prisma/client'
+import { revalidatePath } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
@@ -7,6 +9,10 @@ export async function GET(req: NextRequest) {
   const isFromVercelCron = req.headers
     .get('user-agent')
     ?.startsWith('vercel-cron')
+
+  if (!isFromVercelCron) {
+    return NextResponse.json({ mssg: 'Unauthorized' })
+  }
 
   const now = new Date()
   const yesterdayMidnight = new Date(
@@ -24,8 +30,36 @@ export async function GET(req: NextRequest) {
     select: { dateId: true, date: true, eventId: true },
   })
 
+  const eventsToResync = eventDates.map((eventDate) => eventDate.eventId)
+
+  await db.eventDate.deleteMany({ where: { date: { lt: yesterdayMidnight } } })
+
+  eventsToResync.forEach(async (eventId) => {
+    const event = await db.event.findUniqueOrThrow({
+      where: { eventId },
+      select: { eventId: true, eventDates: { select: { date: true } } },
+    })
+
+    const earliestDate = event.eventDates?.reduce(
+      (earliest: Date | null, eventDate: Pick<EventDate, 'date'>) => {
+        const date = eventDate.date
+
+        if (!earliest) return date
+        return date < earliest ? date : earliest
+      },
+      null as Date | null,
+    )
+
+    if (!earliestDate) {
+      await db.event.delete({ where: { eventId } })
+    } else {
+      await db.event.update({ where: { eventId }, data: { earliestDate } })
+    }
+  })
+
+  revalidatePath('/')
+
   return NextResponse.json({
-    data: eventDates,
-    isFromVercelCron,
+    data: { deletedCount: eventsToResync.length },
   })
 }
