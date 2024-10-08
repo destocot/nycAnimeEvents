31b80689012339d3457158df0677c244f4790d6d
Khurram Ali <khurramcali@gmail.com>
2024-10-08 00:50:39 -0400

    cron test 2

--- app/api/events/cleanup/route.ts
+++ app/api/events/cleanup/route.ts
@@ -3,16 +3,25 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
   console.log('Cleaning up events...')
+  console.log('req', req)
 
   const now = new Date()
+  const yesterdayMidnight = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() - 1,
+  )
 
   const eventDates = await db.eventDate.findMany({
     where: {
       date: {
-        lt: now,
+        lt: yesterdayMidnight,
       },
     },
+    select: { dateId: true, date: true, eventId: true },
   })
 
-  return NextResponse.json({ data: eventDates })
+  return NextResponse.json({
+    data: eventDates,
+  })
 }
